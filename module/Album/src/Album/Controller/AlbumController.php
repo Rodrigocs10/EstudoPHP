f7720c0a790b0f3fd58c70bde0deb9ae2ff1ed38
Rodrigo Cabral <rodrigocs10@gmail.com>
2014-09-10 13:04:55 -0300

    Mudança de estrutura para o tutorial do Zend get started

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -1,27 +1,24 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/Album for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Album\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
 
-class AlbumController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        return array();
-    }
+ class AlbumController extends AbstractActionController
+ {
+     public function indexAction()
+     {
+     }
 
-    public function fooAction()
-    {
-        // This shows the :controller and :action parameters in default route
-        // are working when you browse to /album/album/foo
-        return array();
-    }
-}
+     public function addAction()
+     {
+     }
+
+     public function editAction()
+     {
+     }
+
+     public function deleteAction()
+     {
+     }
+ }
